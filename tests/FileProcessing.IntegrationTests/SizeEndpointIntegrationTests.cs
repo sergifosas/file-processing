5878e9e547e75e49c24e73a8dd4e323de946174c
sergifosas <u1973794@campus.udg.edu>
2026-08-23 23:36:17 +0200

    feat: add size integration tests

---
+++ tests/FileProcessing.IntegrationTests/SizeEndpointIntegrationTests.cs
@@ -0,0 +1,58 @@
+using System.Net;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace FileProcessing.IntegrationTests;
+
+public class SizeEndpointIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public SizeEndpointIntegrationTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory.WithWebHostBuilder(builder => builder.UseEnvironment("Testing"));
+    }
+
+    [Fact]
+    public async Task Process_WithValidFile_ReturnsOk()
+    {
+        var client = _factory.CreateClient();
+
+        using var content = new MultipartFormDataContent();
+        var fileContent = new ByteArrayContent(System.Text.Encoding.UTF8.GetBytes("contenido de prueba"));
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+        content.Add(fileContent, "file", "test.txt");
+
+        var response = await client.PostAsync("/files/size", content);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Process_WithEmptyFile_ReturnsBadRequest()
+    {
+        var client = _factory.CreateClient();
+
+        using var content = new MultipartFormDataContent();
+        var fileContent = new ByteArrayContent(Array.Empty<byte>());
+        fileContent.Headers.ContentType = new MediaTypeHeaderValue("text/plain");
+        content.Add(fileContent, "file", "empty.txt");
+
+        var response = await client.PostAsync("/files/size", content);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task Process_WithoutFile_ReturnsBadRequest()
+    {
+        var client = _factory.CreateClient();
+
+        using var content = new MultipartFormDataContent();
+
+        var response = await client.PostAsync("/files/size", content);
+
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
+}
